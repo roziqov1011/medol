69a9b417541eb03d21c6db560d7ae79eee1cd30a
roziqov1011 <roziqov1011@mail.com>
2023-08-25 14:49:02 +0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Production from './components/Production/Production';
+import Services from './components/Services/Services';
+
+function App() {
+  return (
+      <>
+      <Header/>
+      <Hero/>
+      <Production/>
+      <Services/>
+      </>
+  );
+}
+
+export default App;
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Services.scss'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// import required modules
+import { FreeMode, Pagination } from 'swiper/modules';
+
+function Services() {
+  return (
+    <section className='services'>
+        <h2>УСЛУГИ</h2>
+        <div className="services__slider">
+            <div className="container">
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                freeMode={true}
+                pagination={{
+                clickable: true,
+                }}
+                modules={[FreeMode, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+            </Swiper>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
